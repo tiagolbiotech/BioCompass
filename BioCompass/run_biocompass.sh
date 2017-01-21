9c638fcea9430a0907f9ba9db9bca48e0cde1de1
tiagolbiotech <tferreir@ucsd.edu>
2017-01-21 12:38:04 -0800

    update

--- BioCompass/run_biocompass.sh
+++ BioCompass/run_biocompass.sh
@@ -11,15 +11,15 @@ my_db=`echo /Users/Tiago/Desktop/BioCompass/my_database_clusters`
 find ../antiSMASH_input -maxdepth 1 -mindepth 1 -type d -exec sh -c "fc=\$(find '{}' -name "*cluster*.gbk" \
  | wc -l); echo \"\$fc\t{}\"" \; | sort -nr | cut -d / -f3 > genome_list.txt
 
-#before executing BioCompass, we need to rename all gbk files at the inputs 
-# in order to insert their cluster number in the accession field
-for i in `cat genome_list.txt`; do
-	genome=$i
-	gbk_list=`ls $rootpath/antiSMASH_input/$genome/*cluster*.gbk | xargs -n1 basename | cat`
-	for j in $gbk_list; do
-		python rename_gbk.py $rootpath/antiSMASH_input/$genome/$j $genome
-	done
-done
+# #before executing BioCompass, we need to rename all gbk files at the inputs 
+# # in order to insert their cluster number in the accession field
+# for i in `cat genome_list.txt`; do
+# 	genome=$i
+# 	gbk_list=`ls $rootpath/antiSMASH_input/$genome/*cluster*.gbk | xargs -n1 basename | cat`
+# 	for j in $gbk_list; do
+# 		python rename_gbk.py $rootpath/antiSMASH_input/$genome/$j $genome
+# 	done
+# done
 
 #now, we can execute BioCompass for each input, using the following loop
 for i in `cat genome_list.txt`; do
@@ -38,8 +38,9 @@ for i in `cat genome_list.txt`; do
 			mv $file2 $file2.no
 	        done < $rootpath/BioCompass/exclude_list.txt;
 	fi
+	rm $rootpath/BioCompass/exclude_list.txt
 	make INPUTDIR='/Users/Tiago/Desktop/BioCompass/antiSMASH_input' REFNAME=$genome \
-		MULTIGENEBLASTDIR='/Users/Tiago/Desktop/BioCompass/multigeneblast' CUSTOMDB=$my_db TESTING=1 PART1
+		MULTIGENEBLASTDIR='/Users/Tiago/Desktop/BioCompass/multigeneblast' CUSTOMDB=$my_db PART1
 	for j in $gbk_list; do
 		rm $rootpath/outputs/database_clusters/$j
 	done
@@ -48,15 +49,19 @@ for i in `cat genome_list.txt`; do
 	if [ ! -d $rootpath/merged_results/corrupted_files ]; then mkdir -p $rootpath/merged_results/corrupted_files; \
 	    mv $corrupted $rootpath/merged_results/corrupted_files ; else mv $corrupted $rootpath/merged_results/corrupted_files; fi
 	make INPUTDIR='/Users/Tiago/Desktop/BioCompass/antiSMASH_input' REFNAME=$genome \
-		MULTIGENEBLASTDIR='/Users/Tiago/Desktop/BioCompass/multigeneblast' CUSTOMDB=$my_db TESTING=1 ALL
+		MULTIGENEBLASTDIR='/Users/Tiago/Desktop/BioCompass/multigeneblast' CUSTOMDB=$my_db ALL
 	[ -f $rootpath/merged_results/merged_edges_best_itineration.txt ] && tail -n +2 \
 		$rootpath/outputs/mgb_result/*_edges_best_itineration.txt >> $rootpath/merged_results/merged_edges_best_itineration.txt \
 		|| cat $rootpath/outputs/mgb_result/*_edges_best_itineration.txt > $rootpath/merged_results/merged_edges_best_itineration.txt
 	mv $rootpath/outputs $rootpath/outputs_$genome
 	mv $rootpath/outputs_$genome/database_clusters/*.gbk $my_db
-	rm -r $rootpath/outputs_$genome/database_clusters/
+	# rm -r $rootpath/outputs_$genome/database_clusters/
 done
 
-
+##rename all files back to normal:
+# for file in $(find ../antiSMASH_input/ -name "*.no")
+# do 
+#   mv $file `echo $file | sed s/.no//`
+# done
 
 
